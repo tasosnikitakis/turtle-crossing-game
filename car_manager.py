b6241237add9b0f08a4a31fb6858deb17fcbee7c
tasos <tasosnikic@gmail.com>
2023-03-12 12:13:52 +0200

    Player generation and random car generation

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,33 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.all_cars = []
+
+    def random_car_creation(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 6:
+            new_ycor = random.randrange(-250, 250, STARTING_MOVE_DISTANCE)
+            x_cor = 300
+            position = (x_cor, new_ycor)
+            new_car = Turtle("square")
+            new_car.penup()
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.color(random.choice(COLORS))
+            new_car.goto(position)
+            self.all_cars.append(new_car)
+
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
+
+
+
+
--- main.py
+++ main.py
@@ -8,9 +8,26 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+#Player creation
+player = Player()
+
+#Cars creation
+car_manager = CarManager()
+
+#Player Movement Code
+screen.listen()
+screen.onkey(player.move_up, "Up")
+
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.random_car_creation()
+    car_manager.move_cars()
+
+
+
+
 
 
